045b7aa897b25d89e4b39c78583770a904396a54
luanavfg <luanavefago1@gmail.com>
2024-02-27 09:13:25 -0300

    feat: add answer question use case test

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest'
+import { Answer } from '../entities/answer'
+import { AnswerQuestionUseCase } from './answer-question'
+
+test('create an answer', () => {
+  const answerQuestion = new AnswerQuestionUseCase()
+
+  const answer = answerQuestion.execute({
+    content: 'Nova resposta',
+    instructorId: '1',
+    questionId: '1'
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
+})
