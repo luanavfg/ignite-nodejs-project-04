e0622bd2565ddc6336f53cd7155caac1e5ad6eef
luanavfg <luanavefago1@gmail.com>
2024-02-28 08:18:49 -0300

    adjust: adjust test using fake repository

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,11 +1,18 @@
 import { expect, test } from 'vitest'
 import { Answer } from '../entities/answer'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
 
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer): Promise<void>  => {
+    return
+  }
+}
 
-  const answer = answerQuestion.execute({
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
     content: 'Nova resposta',
     instructorId: '1',
     questionId: '1'
