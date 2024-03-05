38112a38903ed75724a616b2de6e3081bbe4b043
luanavfg <luanavefago1@gmail.com>
2024-03-05 09:05:46 -0300

    feat: add getters in question entity

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,6 +2,7 @@ import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Optional } from "../types/optional"
+import dayjs from "dayjs"
 
 interface QuestionProps {
   authorId: UniqueEntityID
@@ -14,6 +15,38 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  get title() {
+    return this.props.title
+  }  
+  
+  get content() {
+    return this.props.content
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }  
+  
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNewQuestion(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+  
   static create(
     props: Optional<QuestionProps, 'createdAt'>, 
     id?: UniqueEntityID
