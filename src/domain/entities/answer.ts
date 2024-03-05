e59c0b68af020a997632ee6fc2eb07c903bfaf13
luanavfg <luanavefago1@gmail.com>
2024-03-05 09:05:38 -0300

    feat: add getters in answer entity

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -15,6 +15,26 @@ export class Answer extends Entity<AnswerProps>{
     return this.props.content
   }
 
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
   static create(
     props: Optional<AnswerProps, 'createdAt'>, 
     id?: UniqueEntityID
