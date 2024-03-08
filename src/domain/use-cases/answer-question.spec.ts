9159396b49212ac9b846ef033d350655a1f420df
luanavfg <luanavefago1@gmail.com>
2024-03-08 09:06:22 -0300

    fix: eslint fix in use cases

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,11 +1,8 @@
-import { Answer } from '../entities/answer'
 import { AnswerQuestionUseCase } from './answer-question'
 import { AnswersRepository } from '../repositories/answers-repository'
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer): Promise<void>  => {
-    return
-  }
+  create: async (): Promise<void> => {},
 }
 
 test('create an answer', async () => {
@@ -14,7 +11,7 @@ test('create an answer', async () => {
   const answer = await answerQuestion.execute({
     content: 'Nova resposta',
     instructorId: '1',
-    questionId: '1'
+    questionId: '1',
   })
 
   expect(answer.content).toEqual('Nova resposta')
