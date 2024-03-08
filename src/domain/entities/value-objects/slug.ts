8828e832a98c5ed79cb6c54d58faba591200aecc
luanavfg <luanavefago1@gmail.com>
2024-03-08 09:06:30 -0300

    fix: eslint additional fix

--- src/domain/entities/value-objects/slug.ts
+++ src/domain/entities/value-objects/slug.ts
@@ -7,15 +7,15 @@ export class Slug {
 
   /**
    * Recieves a string and normalize it as a slug.
-   * 
+   *
    * Example: "An example title" => "an-example-title"
-   * 
+   *
    * @param text {string}
-   * 
+   *
    */
   static createFromText(text: string) {
     const slugText = text
-      .normalize("NFKD")
+      .normalize('NFKD')
       .toLowerCase()
       .trim()
       .replace(/\s+/g, '-')
