eae3d8479cfaadde3e95839a6fb9258d364280f9
luanavfg <luanavefago1@gmail.com>
2024-03-15 08:15:19 -0300

    feat: add findById and delete methods in repositories

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -6,7 +6,27 @@ export class InMemoryQuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  async findById(id: string) {
+    const questionComment = this.items.find(
+      (comment) => comment.id.toString() === id,
+    )
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
   async create(questionComment: QuestionComment): Promise<void> {
     this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
