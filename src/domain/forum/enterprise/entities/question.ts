34302aed2696c7301f017198025a3af83b64db51
Luana <luanavefago1@gmail.com>
2024-05-13 21:16:27 -0300

    adjust: adjust bestAnswerId setter in question entity

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,9 +1,10 @@
 import { AggregateRoot } from "@/core/entities/aggregate-root";
-import { Slug } from "./value-objects/slug";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
 import dayjs from "dayjs";
+import { QuestionBestAnswerChosenEvent } from "../events/question-best-answer-chosen-event";
 import { QuestionAttachmentList } from "./question-attachment-list";
+import { Slug } from "./value-objects/slug";
 
 export interface QuestionProps {
   authorId: UniqueEntityID;
@@ -79,6 +80,19 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    if (bestAnswerId === undefined) {
+      return;
+    }
+
+    if (
+      this.props.bestAnswerId === undefined ||
+      !bestAnswerId.equals(this.props.bestAnswerId)
+    ) {
+      this.addDomainEvent(
+        new QuestionBestAnswerChosenEvent(this, bestAnswerId),
+      );
+    }
+
     this.props.bestAnswerId = bestAnswerId;
 
     this.touch();
